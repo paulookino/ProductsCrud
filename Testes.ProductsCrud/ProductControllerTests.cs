63823559c5d9220b5da06ad20081691017c54f4f
Paulo Itsuo Almeida Okino <paulo.okino@fcamara.com.br>
2024-02-17 15:58:31 -0300

    Testes.ProductsCrud

--- Testes.ProductsCrud/ProductControllerTests.cs
+++ Testes.ProductsCrud/ProductControllerTests.cs
@@ -1,19 +1,213 @@
-﻿using Business.Models;
-using Business.Services;
+﻿using Business.Interfaces.Services;
+using Business.Models;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using NUnit.Framework;
 using ProductsCrud.Controllers;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Testes.ProductsCrud
 {
-
     [TestFixture]
     public class ProductControllerTests
     {
-    }
+        private ProductController productController;
+        private Mock<IProductService> productServiceMock;
 
+        [SetUp]
+        public void Setup()
+        {
+            productServiceMock = new Mock<IProductService>();
+            productController = new ProductController(productServiceMock.Object);
+        }
+
+        [Test]
+        public async Task Index_ReturnsViewWithProducts()
+        {
+            // Arrange
+            var expectedProducts = new List<Product> { new Product { Id = 1, Name = "Product1" }, new Product { Id = 2, Name = "Product2" } };
+            productServiceMock.Setup(repo => repo.GetProductsAsync()).ReturnsAsync(expectedProducts);
+
+            // Act
+            var result = await productController.Index();
+
+            // Assert
+            Assert.IsInstanceOf<ViewResult>(result);
+            var viewResult = (ViewResult)result;
+            Assert.IsInstanceOf<IEnumerable<Product>>(viewResult.Model);
+            var model = (IEnumerable<Product>)viewResult.Model;
+            Assert.AreEqual(expectedProducts.Count, model.Count());
+        }
+
+
+        [Test]
+        public async Task Details_ProductExists_ReturnsViewWithProduct()
+        {
+            // Arrange
+            int productId = 1;
+            var expectedProduct = new Product { Id = productId, Name = "Product1" };
+            productServiceMock.Setup(repo => repo.GetProductByIdAsync(productId)).ReturnsAsync(expectedProduct);
+
+            // Act
+            var result = await productController.Details(productId);
+
+            // Assert
+            Assert.IsInstanceOf<ViewResult>(result);
+            var viewResult = (ViewResult)result;
+            Assert.IsInstanceOf<Product>(viewResult.Model);
+            var model = (Product)viewResult.Model;
+            Assert.AreEqual(expectedProduct, model);
+        }
+
+        [Test]
+        public async Task Details_ProductDoesNotExist_ReturnsNotFound()
+        {
+            // Arrange
+            int productId = 1;
+            productServiceMock.Setup(repo => repo.GetProductByIdAsync(productId)).ReturnsAsync((Product)null);
+
+            // Act
+            var result = await productController.Details(productId);
+
+            // Assert
+            Assert.IsInstanceOf<NotFoundResult>(result);
+        }
+
+        [Test]
+        public void Create_ReturnsView()
+        {
+            // Act
+            var result = productController.Create();
+
+            // Assert
+            Assert.IsInstanceOf<ViewResult>(result);
+        }
+
+        [Test]
+        public async Task Create_ValidModelState_RedirectsToIndex()
+        {
+            // Arrange
+            var newProduct = new Product { Name = "New Product" };
+
+            // Act
+            var result = await productController.Create(newProduct);
+
+            // Assert
+            
+            Assert.IsInstanceOf<RedirectToActionResult>(result);
+            var redirectToActionResult = (RedirectToActionResult)result;
+            Assert.AreEqual("Index", redirectToActionResult.ActionName);
+        }
+
+        [Test]
+        public async Task Create_InvalidModelState_ReturnsView()
+        {
+            // Arrange
+            var newProduct = new Product { /* ModelState is invalid */ };
+            productController.ModelState.AddModelError("Name", "Name is required");
+
+            // Act
+            var result = await productController.Create(newProduct);
+
+            // Assert
+            Assert.IsInstanceOf<ViewResult>(result);
+            var viewResult = (ViewResult)result;
+            Assert.IsInstanceOf<Product>(viewResult.Model);
+        }
+
+        [Test]
+        public async Task Edit_ProductExists_ReturnsViewWithProduct()
+        {
+            // Arrange
+            var existingProductId = 1;
+            var expectedProduct = new Product { Id = existingProductId, Name = "ExistingProduct" };
+            productServiceMock.Setup(repo => repo.GetProductByIdAsync(existingProductId)).ReturnsAsync(expectedProduct);
+
+            // Act
+            var result = await productController.Edit(existingProductId);
+
+            // Assert
+
+            Assert.IsInstanceOf<ViewResult>(result);
+            var viewResult = (ViewResult)result;
+            Assert.IsInstanceOf<Product>(viewResult.Model);
+            var model = (Product)viewResult.Model;
+            Assert.AreEqual(expectedProduct, model);
+        }
+
+        [Test]
+        public async Task Edit_ProductDoesNotExist_ReturnsNotFound()
+        {
+            // Arrange
+            var nonExistingProductId = 999;
+            productServiceMock.Setup(repo => repo.GetProductByIdAsync(nonExistingProductId)).ReturnsAsync((Product)null);
+
+            // Act
+            var result = await productController.Edit(nonExistingProductId);
+
+            // Assert
+            Assert.IsInstanceOf<NotFoundResult>(result);
+        }
+
+        [Test]
+        public async Task Edit_ValidModelState_RedirectsToIndex()
+        {
+            // Arrange
+            var updatedProduct = new Product { Id = 1, Name = "Updated Product" };
+
+            // Act
+            var result = await productController.Edit(updatedProduct);
+
+            // Assert
+
+            Assert.IsInstanceOf<RedirectToActionResult>(result);
+            var redirectToActionResult = (RedirectToActionResult)result;
+            Assert.AreEqual("Index", redirectToActionResult.ActionName);
+        }
+
+        [Test]
+        public async Task Delete_ProductExists_ReturnsViewWithProduct()
+        {
+            // Arrange
+            var existingProductId = 1;
+            var expectedProduct = new Product { Id = existingProductId, Name = "ExistingProduct" };
+            productServiceMock.Setup(repo => repo.GetProductByIdAsync(existingProductId)).ReturnsAsync(expectedProduct);
+
+            // Act
+            var result = await productController.Delete(existingProductId);
+
+            // Assert
+            Assert.IsInstanceOf<ViewResult>(result);
+            var ViewResult = (ViewResult)result;
+            Assert.AreEqual(expectedProduct, ViewResult.Model);
+        }
+
+        [Test]
+        public async Task Delete_ProductDoesNotExist_ReturnsNotFound()
+        {
+            // Arrange
+            var nonExistingProductId = 999;
+            productServiceMock.Setup(repo => repo.GetProductByIdAsync(nonExistingProductId)).ReturnsAsync((Product)null);
+
+            // Act
+            var result = await productController.Delete(nonExistingProductId);
+
+            // Assert
+            Assert.IsInstanceOf<NotFoundResult>(result);
+        }
+
+        [Test]
+        public async Task DeleteConfirmed_ProductExists_RedirectsToIndex()
+        {
+            // Arrange
+            var existingProductId = 1;
+
+            // Act
+            var result = await productController.DeleteConfirmed(existingProductId);
+
+            // Assert
+            Assert.IsInstanceOf<RedirectToActionResult>(result);
+            var redirectToActionResult = (RedirectToActionResult)result;
+            Assert.AreEqual("Index", redirectToActionResult.ActionName);
+        }
+    }
 }
--- Testes.ProductsCrud/UserControllerTests.cs
+++ Testes.ProductsCrud/UserControllerTests.cs
@@ -1,5 +1,6 @@
 ﻿using Business.Interfaces.Services;
 using Business.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using NUnit.Framework;
@@ -18,38 +19,59 @@ public class UserControllerTests
     {
         _userServiceMock = new Mock<IUserService>();
         _controller = new UserController(_userServiceMock.Object);
+        _controller.ControllerContext = new ControllerContext() { HttpContext = new DefaultHttpContext() };
     }
 
     [Test]
-    public void CreateUser_WithInvalidModel_ReturnsViewWithModelError()
+    public void CreateUser_ReturnsView()
     {
-        // Arrange
-        var invalidUser = new User { };
-        _controller.ModelState.AddModelError("UserName", "Username is required");
-
         // Act
-        var result = _controller.CreateUser(invalidUser) as ViewResult;
+        var result = _controller.CreateUser();
 
         // Assert
-        Assert.IsNotNull(result);
-        Assert.AreEqual(invalidUser, result.Model);
-        _userServiceMock.Verify(u => u.CreateUser(It.IsAny<User>()), Times.Never);
+        Assert.IsInstanceOf<ViewResult>(result);
     }
 
     [Test]
-    public void Login_WithInvalidUser_ReturnsViewWithModelError()
+    public void CreateUser_InvalidModel_ReturnsView()
     {
         // Arrange
         var invalidUser = new User { };
-        _userServiceMock.Setup(u => u.ValidateUser(invalidUser)).Returns(false);
+        _controller.ModelState.AddModelError("Username", "Username is required");
 
         // Act
-        var result = _controller.Login(invalidUser) as ViewResult;
+        var result = _controller.CreateUser(invalidUser);
 
         // Assert
-        Assert.IsNotNull(result);
-        Assert.AreEqual(invalidUser, result.Model);
-        Assert.AreEqual("Invalid username or password", _controller.ModelState[""].Errors.First().ErrorMessage);
-        _userServiceMock.Verify(u => u.ValidateUser(invalidUser), Times.Once);
+
+        Assert.IsInstanceOf<ViewResult>(result);
+        Assert.IsInstanceOf<User>(((ViewResult)result).Model);
+    }
+
+    [Test]
+    public void Login_ReturnsView()
+    {
+        // Act
+        var result = _controller.Login();
+
+        // Assert
+        Assert.IsInstanceOf<ViewResult>(result);
+    }
+
+    [Test]
+    public void Login_InvalidUser_ReturnsViewWithModelError()
+    {
+        // Arrange
+        var invalidUser = new User { Username  = "username" };
+        _userServiceMock.Setup(service => service.ValidateUser(invalidUser)).Returns(false);
+
+        // Act
+        var result = _controller.Login(invalidUser);
+
+        // Assert
+        Assert.IsInstanceOf<ViewResult>(result);
+        Assert.IsInstanceOf<User>(((ViewResult)result).Model);
+        Assert.IsFalse(_controller.ModelState.IsValid);
+        Assert.IsTrue(_controller.ModelState.ContainsKey(string.Empty));
     }
 }
